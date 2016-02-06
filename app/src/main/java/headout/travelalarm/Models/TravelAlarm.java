90fb3b969f84d8bb560126f66731bdb117b3506e
lokesh.pogeyan <lokesh@pogeyan.com>
2016-02-07 02:27:07 +0530

    pushed the core changes to the DB

--- app/src/main/java/headout/travelalarm/Models/TravelAlarm.java
+++ app/src/main/java/headout/travelalarm/Models/TravelAlarm.java
@@ -1,82 +1,50 @@
 package headout.travelalarm.Models;
 
-import android.content.ContentValues;
-import android.database.Cursor;
-
-import java.util.ArrayList;
-import java.util.List;
-import headout.travelalarm.Db.DB;
-import rx.functions.Func1;
-
 /**
  * Created by Lokesh on 07-02-2016.
  */
 public class TravelAlarm {
 
-    public static final String TABLE = "alarms";
+    public long Id;
+    public float Start;
+    public float Dest;
+    public int Kms;
+    public int ETA;
+    public int WakeUpType;
 
-    public static final String ID = "_id";
-    public static final String START = "start";
-    public static final String DEST = "dest";
-    public static final String KMS ="kms";
-    public static final String ETA="eta";
-    public static final String WAKEUPTYPE ="wakeuptype";
-    public static Func1<Cursor, List<TravelAlarm>> MAP = new Func1<Cursor, List<TravelAlarm>>() {
-        @Override public List<TravelAlarm> call(final Cursor cursor) {
-            try {
-                List<TravelAlarm> values = new ArrayList<>(cursor.getCount());
+    public TravelAlarm setId(long id)
+    {
+        this.Id = id;
+        return this;
+    }
 
-                while (cursor.moveToNext()) {
-                    long id = DB.getLong(cursor, ID);
-                    String name = DB.getString(cursor, NAME);
-                    boolean archived = DB.getBoolean(cursor, ARCHIVED);
-                    values.add(new AutoParcel_TodoList(id, name, archived));
-                }
-                return values;
-            } finally {
-                cursor.close();
-            }
-        }
-    };
+    public TravelAlarm setStart(float start)
+    {
+        this.Start = start;
+        return this;
+    }
 
-    public static final class Builder {
-        private final ContentValues values = new ContentValues();
+    public TravelAlarm setDest(float dest)
+    {
+        this.Dest = dest;
+        return this;
+    }
 
-        public Builder id(long id) {
-            values.put(ID, id);
-            return this;
-        }
+    public TravelAlarm setKms(int kms)
+    {
+        this.Kms = kms;
+        return this;
+    }
 
-        public Builder start(Float start) {
-            values.put(START, start);
-            return this;
-        }
+    public TravelAlarm setETA(int eta)
+    {
+        this.ETA = eta;
+        return this;
+    }
 
-        public Builder dest(Float dest) {
-            values.put(DEST, dest);
-            return this;
-        }
-
-        public Builder kms(Integer kms)
-        {
-            values.put(KMS, kms);
-            return this;
-        }
-
-        public Builder eta(Integer eta)
-        {
-            values.put(ETA, eta);
-            return this;
-        }
-
-        public Builder wakeuptype(Integer wakeUpType)
-        {
-            values.put(WAKEUPTYPE, wakeUpType);
-            return this;
-        }
-
-        public ContentValues build() {
-            return values;
-        }
+    public TravelAlarm setWakeUpType(int wk)
+    {
+        this.WakeUpType = wk;
+        return this;
     }
 }
---
+++ app/src/main/java/headout/travelalarm/Models/TravelAlarmBuilder.java
@@ -0,0 +1,87 @@
+package headout.travelalarm.Models;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import headout.travelalarm.Db.DB;
+import rx.functions.Func1;
+
+/**
+ * Created by Lokesh on 07-02-2016.
+ */
+
+public class TravelAlarmBuilder {
+
+    public static final String TABLE = "alarms";
+
+    public static final String ID = "_id";
+    public static final String START = "start";
+    public static final String DEST = "dest";
+    public static final String KMS = "kms";
+    public static final String ETA = "eta";
+    public static final String WAKEUPTYPE = "wakeuptype";
+
+
+    public static Func1<Cursor, List<TravelAlarm>> MAP = new Func1<Cursor, List<TravelAlarm>>() {
+        @Override
+        public List<TravelAlarm> call(final Cursor cursor) {
+            try {
+                List<TravelAlarm> values = new ArrayList<>(cursor.getCount());
+
+                while (cursor.moveToNext()) {
+                    long id = DB.getLong(cursor, ID);
+                    float start = DB.getFloat(cursor, START);
+                    float dest = DB.getFloat(cursor, DEST);
+                    int kms = DB.getInt(cursor, KMS);
+                    int eta = DB.getInt(cursor, ETA);
+                    int wakeType = DB.getInt(cursor,WAKEUPTYPE);
+                    values.add(new TravelAlarm().setId(id).setStart(start).setDest(dest).setKms(kms).setETA(eta).setWakeUpType(wakeType));
+                }
+                return values;
+            } finally {
+                cursor.close();
+            }
+        }
+    };
+
+    public static final class Builder {
+        private final ContentValues values = new ContentValues();
+
+        public Builder id(long id) {
+            values.put(ID, id);
+            return this;
+        }
+
+        public Builder start(Float start) {
+            values.put(START, start);
+            return this;
+        }
+
+        public Builder dest(Float dest) {
+            values.put(DEST, dest);
+            return this;
+        }
+
+        public Builder kms(Integer kms) {
+            values.put(KMS, kms);
+            return this;
+        }
+
+        public Builder eta(Integer eta) {
+            values.put(ETA, eta);
+            return this;
+        }
+
+        public Builder wakeuptype(Integer wakeUpType) {
+            values.put(WAKEUPTYPE, wakeUpType);
+            return this;
+        }
+
+        public ContentValues build() {
+            return values;
+        }
+    }
+}
